b13400f626592b77a7b4ae487aa8c88808402b80
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-23 15:03:20 +0545

    changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <div>
+
+      <h1 className="text-2xl font-bold text-red-400">Hello jee</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non facere soluta ducimus iste veritatis laudantium delectus commodi tempora odio officiis molestias magnam provident animi, minus consequatur cupiditate itaque eligendi quis?</p>
+    </div>
+  )
+}
