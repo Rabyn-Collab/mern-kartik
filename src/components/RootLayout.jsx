a570ac6768524f2b9773deeacd70dbafd64db9f4
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-30 15:37:47 +0545

    changes

---
+++ src/components/RootLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router";
+import Header from "./Header";
+
+export default function RootLayout() {
+
+
+
+  return (
+    <div>
+
+      <Header />
+
+      <Outlet />
+
+
+
+
+
+
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,11 @@
-import { NavLink, Outlet } from "react-router";
-import Header from "../../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <h1>This is Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores totam adipisci labore odit ab dolorem? Nisi, in tempora quis, itaque voluptatibus nostrum ipsa beatae odio magni iure commodi sapiente autem?</p>
-      <NavLink to={'/page1'}>Page1</NavLink>
-      <NavLink to={'/page2'}>Page2</NavLink>
 
-      <Outlet />
+      <h1 className="text-midnight">This is Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores totam adipisci labore odit ab dolorem? Nisi, in tempora quis, itaque voluptatibus nostrum ipsa beatae odio magni iure commodi sapiente autem?</p>
+
     </div>
   )
 }
