9bb30d9f008d307b40177a4d16fe577e4b14e825
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-30 15:52:13 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,13 @@
 import { NavLink } from "react-router";
+import { FaAlignJustify } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <div className="bg-black text-white px-5 py-2 flex items-baseline justify-between">
 
-      <h1 className="text-2xl">React Router</h1>
-
-      <nav className="flex gap-5">
-
-        <NavLink to={'/about'}>About</NavLink>
-        <NavLink to={'/contact'}>Contact</NavLink>
+      <FaAlignJustify size={20} />
 
 
-
-      </nav>
 
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
+import DisplaySection from "./DisplaySection";
 
 export default function Home() {
   return (
     <div>
 
-      <h1 className="text-midnight">This is Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores totam adipisci labore odit ab dolorem? Nisi, in tempora quis, itaque voluptatibus nostrum ipsa beatae odio magni iure commodi sapiente autem?</p>
+      <DisplaySection />
+
+
 
     </div>
   )
