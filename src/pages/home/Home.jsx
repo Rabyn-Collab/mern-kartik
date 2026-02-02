01e5c4e66447ab8076c93d9e2e7951ef6ea9e9eb
Rabyn-Collab <babynshrestha64@gmail.com>
2026-02-02 15:51:25 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux"
+import { Button } from "../../components/ui/button.jsx";
+import { EditIcon, TrashIcon } from "lucide-react";
 
 export default function Home() {
 
@@ -7,6 +9,22 @@ export default function Home() {
   console.log(todos);
 
   return (
-    <div>Home</div>
+    <div className="grid gap-5 grid-cols-3 p-5">
+      {todos.map((todo) => (
+        <div key={todo.id} className="shadow-xs p-3">
+
+          <h1>{todo.email}</h1>
+          <p>{todo.gender}</p>
+          <h3>{todo.country}</h3>
+
+          <div className="mt-3 flex gap-5">
+            <Button variant="outline"> <EditIcon /> </Button>
+            <Button variant="outline"> <TrashIcon /> </Button>
+          </div>
+
+
+        </div>
+      ))}
+    </div>
   )
 }
