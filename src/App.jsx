a9f6891ffcff403e663bfca6ddd1597f71ba629c
Rabyn-Collab <babynshrestha64@gmail.com>
2026-03-09 14:55:13 +0545

    cahnges

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import RootLayout from "./components/RootLayout.jsx";
 import Home from "./features/home/Home.jsx";
 import Login from "./features/auth/Login.jsx";
+import Register from "./features/auth/Register.jsx";
 
 export default function App() {
 
@@ -18,6 +19,10 @@ export default function App() {
         {
           path: 'login',
           element: <Login />
+        },
+        {
+          path: 'register',
+          element: <Register />
         }
 
 
