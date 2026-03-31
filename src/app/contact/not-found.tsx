25162be062638c67c2749bd28de2bf82e7319f34
Rabyn-Collab <babynshrestha64@gmail.com>
2026-03-31 14:39:39 +0545

    first commit

---
+++ src/app/contact/not-found.tsx
@@ -0,0 +1,5 @@
+export default function ContactNotFound() {
+  return (
+    <div>ContactNotFound</div>
+  )
+}
---
+++ src/components/Compo.tsx
@@ -0,0 +1,7 @@
+export default function Compo({ name, age }: { name: string, age: number }) {
+
+
+  return (
+    <div>Compo {age} {name}</div>
+  )
+}
